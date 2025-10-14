8dfeb0003a2590734f6dd11e84bd0c1fb43d4cad
Chris Siah <chinloongsiah@gmail.com>
2025-10-14 17:44:24 +0800

    All done

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 practice assignment 1 for 2025 W41B
 
 This line is added in remote, at 4:41pm.
+Complted the code in
---
+++ spyder/integer python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 14 16:59:53 2025
+
+@author: user
+"""
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
